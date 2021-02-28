887d930fb27ceb77107ab6225d15b8ca6a915edf
olina6 <olina6@gmail.com>
2021-03-01 10:11:31 +1300

    modify 1 March

--- demo.php
+++ demo.php
@@ -1,5 +1,12 @@
 <?php
-function demo(){
-    echo "php";
-
+function familyName($fName){
+    echo "$fName Refsnes.<br>";
 };
+
+familyName("a");
+familyName("b");
+function addNumbers($a, $b){
+    return $a+$b;
+}
+echo addNumbers(1.2,5.2);
+echo addNumbers(1.2,5.2);
